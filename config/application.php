3362454bef3abdcb78ca5057b3ba92a5f1cedd4c
Steven DuBois <srd2725@gmail.com>
2022-08-11 22:24:25 -0500

    change env var names back

--- config/application.php
+++ config/application.php
@@ -64,8 +64,8 @@ Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_D
 /**
  * DB settings
  */
-Config::define('DB_NAME', env('DB_DATABASE'));
-Config::define('DB_USER', env('DB_USERNAME'));
+Config::define('DB_NAME', env('DB_NAME'));
+Config::define('DB_USER', env('DB_USER'));
 Config::define('DB_PASSWORD', env('DB_PASSWORD'));
 Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
 Config::define('DB_CHARSET', 'utf8mb4');
